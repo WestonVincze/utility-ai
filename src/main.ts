cceb2b4866b4699f714be0a2cf1a8e9d7e80c9c4
Weston Vincze <westonvincze@gmail.com>
2024-08-06 01:43:33 -0400

    🏷️ Scaffold types for robust system

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import './style.css'
 
 /** UTILITY AI */
+class UtilityAI<T> {
+  #context: T;
+  updatePeriod: number;
+
+  #rootReasoner: Reasoner<T>;
+
+  constructor(context: T, reasoner: Reasoner<T>, updatePeriod = 1) {
+    this.#context = context;
+    this.updatePeriod = updatePeriod;
+    this.#rootReasoner = reasoner;
+  }
+}
 
 /**
  * REASONERS
@@ -8,6 +20,10 @@ import './style.css'
  * * * first score
  * * * highest score
  */ 
+type Reasoner<T> = {
+  considerations: Consideration<T>;
+  select: (context: T) => Action<T>;
+}
 
 
 /**
@@ -15,18 +31,28 @@ import './style.css'
  * * contains a list of APPRAISALS and ACTIONS
  * * calculates the score which represents numerically the utility of its ACTION
  */ 
+type Consideration<T> = {
+  action: Action<T>;
+  score: number;
+  appraisals?: Appraisal<T>[];
+}
 
 /**
  * APPRAISALS
- * 
  */
+type Appraisal<T> = {
+  getScore: (context: T) => number;
+}
 
 /**
  * ACTIONS
- * 
  */
-
+type Action<T> = {
+  name: string;
+  execute: (context: T) => void;
+}
 
 /**
  * AGENT
  */
+const Agent = {}
