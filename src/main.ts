1f4f9c772b72be7af67d9d4b14ffff6def8ad3d1
Weston Vincze <westonvincze@gmail.com>
2024-08-05 23:56:35 -0400

    🎉 Initialize Utility AI Repo

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import './style.css'
+
+/** UTILITY AI */
+
+/**
+ * REASONERS
+ * * select CONSIDERATION from a list of CONSIDERATIONS
+ * * * first score
+ * * * highest score
+ */ 
+
+
+/**
+ * CONSIDERATIONS
+ * * contains a list of APPRAISALS and ACTIONS
+ * * calculates the score which represents numerically the utility of its ACTION
+ */ 
+
+/**
+ * APPRAISALS
+ * 
+ */
+
+/**
+ * ACTIONS
+ * 
+ */
+
+
+/**
+ * AGENT
+ */
