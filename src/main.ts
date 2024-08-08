ccbf63a620758aefdf8d94bbef82ec8f34cd7a39
Weston Vincze <westonvincze@gmail.com>
2024-08-08 00:58:24 -0400

    🚧 EoD commit - scaffold full system (WIP)

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import './style.css'
 
 /** UTILITY AI */
-class UtilityAI<T> {
-  #context: T;
+class UtilityAI<TContext> {
+  #context: TContext;
   updatePeriod: number;
 
-  #rootReasoner: Reasoner<T>;
+  #rootReasoner: Reasoner<TContext>;
 
-  constructor(context: T, reasoner: Reasoner<T>, updatePeriod = 1) {
+  constructor(context: TContext, reasoner: Reasoner<TContext>, updatePeriod = 1) {
     this.#context = context;
     this.updatePeriod = updatePeriod;
     this.#rootReasoner = reasoner;
@@ -15,44 +15,101 @@ class UtilityAI<T> {
 }
 
 /**
- * REASONERS
+ * REASONERS - TOP LEVEL "ACTION"
  * * select CONSIDERATION from a list of CONSIDERATIONS
  * * * first score
  * * * highest score
  */ 
-type Reasoner<T> = {
-  considerations: Consideration<T>;
-  select: (context: T) => Action<T>;
+interface Reasoner<TContext> {
+  considerations: Consideration<TContext>[];
+  select: (context: TContext) => Action<TContext> | null;
+  addConsideration: (consideration: Consideration<TContext>) => void
+  selectBestConsideration: (context: TContext) => Consideration<TContext> | null;
+  // default consideration + addDefaultConsideration?
 }
 
 
 /**
- * CONSIDERATIONS
+ * CONSIDERATIONS - SUB 
  * * contains a list of APPRAISALS and ACTIONS
  * * calculates the score which represents numerically the utility of its ACTION
  */ 
-type Consideration<T> = {
-  action: Action<T>;
+interface Consideration<TContext> {
+  action: Action<TContext>;
   score: number;
-  appraisals?: Appraisal<T>[];
+  appraisals?: Appraisal<TContext>[];
 }
 
 /**
  * APPRAISALS
+ * * the calculated Utility
+ * * * https://www.desmos.com/calculator
  */
-type Appraisal<T> = {
-  getScore: (context: T) => number;
+interface Appraisal<TContext> {
+  getScore: (context: TContext) => number;
 }
 
 /**
  * ACTIONS
  */
-type Action<T> = {
+interface Action<TContext> {
   name: string;
-  execute: (context: T) => void;
+  execute: (context: TContext) => void;
 }
 
 /**
  * AGENT
+ * * context
+ * * personality
+ * * currentAction
  */
-const Agent = {}
+interface Agent<TContext> {
+  name: string;
+  currentAction: string | null;
+  context: TContext
+}
+
+type ContextualUnitData = {
+  distance: number,
+  health: number,
+  power: number
+}
+
+// implement game state / context
+type Context = {
+  health: number,
+  allies: ContextualUnitData[],
+  enemies: ContextualUnitData[],
+}
+
+
+// build actions
+
+class FleeAction implements Action<Context> {
+  name = "Flee";
+
+  execute(context: Context): void {
+    // fleeing
+  }
+}
+
+// build appraisals
+
+// place action / appraisal combinations into considerations
+
+// place considerations into reasoner
+
+/**
+ * http://www.gameaipro.com/GameAIPro/GameAIPro_Chapter09_An_Introduction_to_Utility_Theory.pdf
+ * The key to understanding Utility theory is to understand the relationship between the input and the output, and being able to describe that resulting curve. Think of it as a "conversion process"
+ * * linear
+ * * quadratic 
+ * * piecewise linear curves
+ * 
+ * Inertia:
+ * * a solution to the "oscillation" problem (AI rapidly switching between actions)
+ * * Possible solutions:
+ * * * add weight to any action that is already engaged
+ * * * cooldowns - apply a strong weight once a decision has been made that can drop off over time
+ * * * stall the decision making system (time based or until action is completed)
+ */
